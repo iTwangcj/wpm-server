132d7f3190e8d5dde4c4c7f8a9f97a670d6b70ac
iTwangcj <wangcj10086@163.com>
2017-12-16 21:05:42 +0800

    update

--- server/server.js
+++ server/server.js
@@ -33,7 +33,10 @@ io.sockets.on('connection', (conn) => {
 			return true;
 		}
 	};
-	
+	conn.on('error', function (err) {
+		console.log('Caught flash policy server socket error: ');
+		console.log(err.stack);
+	});
 	conn.on('login', function (data) {
 		// console.log('client Request login: %s', JSON.stringify(data));
 		if (loginValidate(data)) return;
@@ -50,9 +53,27 @@ io.sockets.on('connection', (conn) => {
 	});
 	conn.on('disconnect', function () {
 		console.log('Server Socket disconnect username: %s', user.username);
-		conn.emit('disconnect');
+		conn.disconnect();
 	});
 });
+
+/**
+ * 获取文件夹下面的所有的文件(包括子文件夹)
+ * @param {String} dir
+ * @returns {Array}
+ */
+const getAllFiles = (dir) => {
+	let AllFiles = [];
+	const iteration = (dirPath) => {
+		const [dirs, files] = _(fs.readdirSync(dirPath)).partition(p => fs.statSync(path.join(dirPath, p)).isDirectory());
+		files.forEach(file => AllFiles.push(path.join(dirPath, file)));
+		for (const _dir of dirs) {
+			iteration(path.join(dirPath, _dir));
+		}
+	};
+	iteration(dir);
+	return AllFiles;
+};
 
 const handleCommand = (conn, params, username) => {
 	let result = '';
@@ -76,14 +97,26 @@ const handleCommand = (conn, params, username) => {
 	})
 	.then(() => {
 		global.cd(userPath);
-		global.exec(`tar -zcf node_modules.tar.gz ${node_modules}`);
-		conn.emit('data', `/${username}/node_modules.tar.gz`);
+		const filePaths = getAllFiles(watchPath);
+		const list = [];
+		for (const filePath of filePaths) {
+			let newFilePath = filePath.split(username)[1];
+			if (newFilePath) {
+				let tmpArr = newFilePath.split(node_modules);
+				tmpArr = tmpArr.slice(1, tmpArr.length);
+				const _filePath = tmpArr.join(node_modules);
+				if (_filePath) {
+					list.push(_filePath);
+				}
+			}
+		}
+		conn.emit('data', { username, list });
 		conn.emit('result', result);
 	})
 	// error catch
 	.catch(e => {
 		global.echo(chalk.red('Build failed. See below for errors.\n'));
 		global.echo(chalk.red(e.stack));
-		process.exit(1);
+		// process.exit(1);
 	});
 };
